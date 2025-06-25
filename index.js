bc3d54ba4ab6e1327d5b034a343bcc3c3c6160db
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2025-06-25 09:44:48 +0500

    test code 6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import connectDB from './config/db.js';
 import userModel from './models/userModel.js';
-// import authRouter from './router/authRouter.js';
+import authRouter from './router/authRouter.js';
 
 connectDB();
 dotenv.config();
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 
-// app.use('/api', authRouter)
+app.use('/api', authRouter)
 
 app.get('/' , async (req , res) =>{
         const usersData = await userModel.find({})
