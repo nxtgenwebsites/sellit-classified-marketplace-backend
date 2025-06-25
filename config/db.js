9e7d546af0aebd4629e53c98f9980a81132eda72
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2025-06-25 09:41:52 +0500

    test code 5

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = () => {
-    mongoose.connect('mongodb+srv://shahbazansari8998:j0jLR5BOulhmoRFs@sellit-pakistan.np0d4lt.mongodb.net/').then(()=> {
+    mongoose.connect('mongodb+srv://shahbazansari8998:aRfKENwql8bxbGH0@portfolio.u4rpgss.mongodb.net/sellit-pakistan').then(()=> {
        console.log('MongoDB connected successfully.');
    }).catch((error) =>{
     console.log(error);
